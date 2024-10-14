2efd5a5f8f99595a2a98563eba5f5670a83ac7ff
Noah Saso <noahsaso@gmail.com>
2024-10-14 00:56:43 -0400

    added delegated VP logic to single and multiple choice proposal modules and removed redundant state

--- contracts/pre-propose/dao-pre-propose-approval-single/src/tests.rs
+++ contracts/pre-propose/dao-pre-propose-approval-single/src/tests.rs
@@ -83,6 +83,7 @@ fn get_default_proposal_module_instantiate(
         },
         close_proposal_on_execution_failure: false,
         veto: None,
+        delegation_module: None,
     }
 }
 
@@ -1702,6 +1703,7 @@ fn test_instantiate_with_zero_native_deposit() {
             },
             close_proposal_on_execution_failure: false,
             veto: None,
+            delegation_module: None,
         }
     };
 
@@ -1771,6 +1773,7 @@ fn test_instantiate_with_zero_cw20_deposit() {
             },
             close_proposal_on_execution_failure: false,
             veto: None,
+            delegation_module: None,
         }
     };
 
